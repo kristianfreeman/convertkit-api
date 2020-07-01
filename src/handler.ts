4f3cd3016fd628b20670734f55c4aaad626c3564
Kristian Freeman <kristian@kristianfreeman.com>
2020-07-01 15:21:15 -0500

    Configure via wrangler.toml

--- src/handler.ts
+++ src/handler.ts
@@ -1,15 +1,16 @@
-export async function handleRequest(
-  event: FetchEvent,
-  config: Config,
-): Promise<Response> {
+const config = {
+  baseUrl: typeof BASE_URL == 'undefined' ? null : BASE_URL,
+  cacheTtl: typeof CACHE_TTL == 'undefined' ? null : CACHE_TTL,
+}
+
+export async function handleRequest(event: FetchEvent): Promise<Response> {
   try {
-    const { baseUrl, cacheTtl } = config
     const { request } = event
     const { pathname, search } = new URL(request.url)
-    const url = new URL(baseUrl + pathname + search)
+    const url = new URL(config.baseUrl + pathname + search)
 
     return await fetch(url.toString(), {
-      ...(cacheTtl ? { cf: { cacheTtl } } : {}),
+      ...(config.cacheTtl ? { cf: { cacheTtl: Number(config.cacheTtl) } } : {}),
       headers: {
         ...request.headers,
       },
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,8 @@
 import { KVNamespace } from '@cloudflare/workers-types'
 
 declare global {
+  const BASE_URL: string
+  const CACHE_TTL: number
   const KV_CACHE: KVNamespace
 
   interface Config {
